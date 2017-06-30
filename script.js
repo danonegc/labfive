56a0095c15ca99572990e7d63f2bd9b68a9be4b2
danonegc <dcureton@gmail.com>
2017-06-30 08:39:00 -0400

    Initial submission of tempconverter lab

---
+++ script.js
@@ -0,0 +1,10 @@
+alert ('hi');
+var f=' degrees Fahrenheit';
+var fTemp=prompt('What is your temperature? In Fahrenheit only please!');
+console.log ('' + fTemp + f);
+console.log ('Your temperature in Celsius is being calculated...');
+var c=' degrees Celsius';
+console.log(((fTemp - 32)*(5/9)) + c);
+function Dice() {
+  document.write('1');
+}
